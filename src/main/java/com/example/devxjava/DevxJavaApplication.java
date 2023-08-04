e37a9f4817645942ecd716eaf749fc38b43ca46c
ntudat <nguyentudat19989@gmail.com>
2023-08-04 15:02:24 +0700

    init

---
+++ src/main/java/com/example/devxjava/DevxJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.devxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevxJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevxJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devxjava/DevxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevxJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
