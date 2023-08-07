01dcc74c660348cf16942cef183ae70bb7a83e9e
kubim1998 <73687451+kubim1998@users.noreply.github.com>
2023-08-07 16:42:58 +0700

    Update DevxJavaApplication.java

--- src/main/java/com/example/devxjava/DevxJavaApplication.java
+++ src/main/java/com/example/devxjava/DevxJavaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevxJavaApplication {
+public class {{ClassName}}Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevxJavaApplication.class, args);
